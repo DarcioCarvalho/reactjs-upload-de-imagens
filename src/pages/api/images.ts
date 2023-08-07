b76f43de5a3b07de354fd0c24f465b366a712b41
DarcioCarvalho <darcio.nuno@gmail.com>
2023-08-07 12:03:40 -0300

    refactor: updated api/images #01

--- src/pages/api/images.ts
+++ src/pages/api/images.ts
@@ -52,6 +52,7 @@ export default async function handler(
   }
 
   if (req.method === 'GET') {
+    console.log('Fauna_Key: ', process.env.FAUNA_API_KEY);
     const { after } = req.query;
 
     const queryOptions = {
