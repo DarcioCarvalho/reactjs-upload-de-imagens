84c61c3323c7d71f0e1ee06b132aaba53925804c
DarcioCarvalho <darcio.nuno@gmail.com>
2023-08-07 12:58:21 -0300

    refactor: updated index #02

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home(): JSX.Element {
         after: pageParam,
       },
     });
-    console.log('data[FetchImages]: ', data);
+    alert('data[FetchImages]: ', data);
     return data;
   }
 
